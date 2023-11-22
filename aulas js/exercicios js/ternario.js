c379a3633316ecb4936bbf54cd84717b9593b925
filiperamgund <ramgund.filipee@gmail.com>
2023-11-21 21:37:58 -0300

    exercicios comparacoes feito

---
+++ aulas js/exercicios js/ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
+
+//
+
+const nome = "Adrinaldo";
+const sobrenome = "Ramgund";
+
+console.log(nome.length >= sobrenome.length ? "Sim" : "Não")
